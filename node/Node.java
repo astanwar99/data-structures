17e3d278844cb68821e2a16d10801b20b03e2cbc
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2020-04-13 17:22:02 +0530

    node package for trees and graphs

---
+++ node/Node.java
@@ -0,0 +1,12 @@
+package Node;
+
+public class Node {
+    public int key;
+    public Node left, right;
+
+    public Node(int key){
+        this.key = key;
+        left = null;
+        right = null;
+    }
+}
