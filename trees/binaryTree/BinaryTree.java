f4c214e48c7ac70288bc51822ab59cd5c47346df
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2020-05-06 21:05:18 +0530

    basic method definition

--- trees/binaryTree/BinaryTree.java
+++ trees/binaryTree/BinaryTree.java
@@ -2,5 +2,17 @@ package trees.binaryTree;
 import trees.node.Node;
 
 public class BinaryTree {
+    private Node root;
 
+    public BinaryTree(){
+        root = null;
+    }
+
+    public void setNode(int num) {
+        this.root = new Node(num);
+    }
+
+    public BinaryTree(int key){
+        root = new Node(key);
+    }
 }
