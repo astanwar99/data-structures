80443955cfaade386f9e4f66c970daa55e6355a3
astanwar99 <as.tanwar3198@gmail.com>
2020-05-14 19:02:16 +0530

    2nd soln

--- Interview/Amazon/k_largest_element_in_array.java
+++ Interview/Amazon/k_largest_element_in_array.java
@@ -16,13 +16,44 @@ class Test{
         for (int i = n - 1; i > n - k - 1 ; i--) {
             System.out.print(arr[i] + " ");            
         }
+        System.out.println();
+    }
 
+    void sol2_temp_array(int[] arr, int k){
+        System.out.println("Temporary array soln - O((n - k)*k)");
+        int n = arr.length;
+        int[] temp = new int[k];
+        int temp_min_index = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < temp.length; j++) {
+                if(min < temp[j]){
+                    min = temp[j];
+                    temp_min_index = j;
+                }
+            }
+
+            if(arr[i] > temp[temp_min_index]){
+                temp[temp_min_index] = arr[i];
+            }
+        }
+
+        for (int i = n - 1; i > n - k - 1 ; i--) {
+            System.out.print(arr[i] + " ");            
+        }
     }
 
     public static void main(String[] args) {
         int[] arr = {1, 23, 12, 9, 30, 2, 50};
         int k = 3;
         Test t = new Test();
+
         t.sol1_bubble_sort(arr, k);
+        t.sol2_temp_array(arr, k);
     }
 }
