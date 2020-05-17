4a654f998cd345ab0ef43772c3ca700492a35bd0
astanwar99 <as.tanwar3198@gmail.com>
2020-05-17 19:43:34 +0530

    sol2

--- Interview/Amazon/pythagorean_triplet_in_array.java
+++ Interview/Amazon/pythagorean_triplet_in_array.java
@@ -22,10 +22,44 @@ class Test{
         return false;
     }
 
+    boolean sol2_sorting(int[] arr) {
+        int[] ar = arr.clone();
+        int a, b, c;
+        int f, l;
+        Arrays.sort(ar);
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = ar[i] * ar[i];
+        }
+
+        for (int i = ar.length - 1; i > 0; i--) {
+            c = ar[i];
+            f = 0;
+            l = i - 1;
+            a = ar[f];
+            b = ar[l];
+            while(b >= a) {
+                a = ar[f];
+                b = ar[l];
+                if(a + b == c){
+                    System.out.println("Triplet found: " + a + ", " + b + ", " + c);
+                    return true;
+                }
+                else if(a + b < c){
+                    f++;
+                }
+                else if(a + b > c){
+                    l--;
+                }
+            }
+        }
+        System.out.println("No triplet found");
+        return false;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 3, 1, 4, 6, 5 };
         Test t = new Test();
         t.sol1_brute_method(arr);
-        // t.sol2_sorting(arr);
+        t.sol2_sorting(arr);
     }
 }
