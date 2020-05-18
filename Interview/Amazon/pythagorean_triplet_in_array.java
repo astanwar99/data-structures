05d793e89cc5ac7ca0851798f3f869fab022688d
astanwar99 <as.tanwar3198@gmail.com>
2020-05-18 19:30:09 +0530

    3rd soln

--- Interview/Amazon/pythagorean_triplet_in_array.java
+++ Interview/Amazon/pythagorean_triplet_in_array.java
@@ -56,10 +56,51 @@ class Test{
         return false;
     }
 
+    boolean sol3_hashing(int[] arr) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+        int max = 0;
+        int[] ar = arr.clone();
+        for (int i = 0; i < ar.length; i++) {
+            max = Math.max(max, ar[i]);
+            hmap.put(ar[i], 0);
+        }
+        for (int i : ar) {
+            hmap.put(i, hmap.get(i) + 1);
+        }
+
+        for(int i = 1; i < max + 1; i++) {
+            if(hmap.get(i) == null)
+                continue;
+            
+            for(int j = 1; j < max + 1; j++) {
+                if(hmap.get(j) == null || (hmap.get(i) == hmap.get(j) && hmap.get(i) < 2)){
+                    continue;
+                }
+
+                // Find c 
+                int val = (int) Math.sqrt(i * i + j * j); 
+    
+                // If c^2 is not a perfect square 
+                if ((val * val) != (i * i + j * j)) 
+                    continue; 
+    
+                // If c exceeds the maximum value 
+                if (val > max) 
+                    continue; 
+                
+                if(hmap.get(val) == 1)
+                    return true;
+
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 3, 1, 4, 6, 5 };
         Test t = new Test();
         t.sol1_brute_method(arr);
         t.sol2_sorting(arr);
+        System.out.println(t.sol3_hashing(arr));
     }
 }
