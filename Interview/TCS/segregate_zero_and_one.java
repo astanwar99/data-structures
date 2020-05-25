4701d3f8f79e709f81c93e52e750e1e8e46aa0d6
astanwar99 <as.tanwar3198@gmail.com>
2020-05-25 21:08:23 +0530

    2nd Ques

---
+++ Interview/TCS/segregate_zero_and_one.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class segregate_zero_and_one{
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 1, 0};
+
+        int l = 0, r = arr.length - 1;
+        
+        while(true) {
+            if(l >= r){
+                break;
+            }
+
+            if(arr[l] == 0){
+                l++;
+            }else if(arr[r] == 1){
+                r--;
+            }else {
+                arr[l] = 0;
+                arr[r] = 1;
+                l++;
+                r--;
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i);
+        }
+    }
+}
