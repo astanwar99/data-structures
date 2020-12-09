4a6b063984bedde6cc7fecbc94df79dd206b17e1
astanwar99 <as.tanwar3198@gmail.com>
2020-12-09 14:31:49 +0530

    remove blocks, add comments

--- Codechef/July20B/CHEFSTR1_chef_and_string.java
+++ Codechef/July20B/CHEFSTR1_chef_and_string.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
 
+//Changes for testing branches(git)
 public class CHEFSTR1_chef_and_string{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int T = s.nextInt();
-
         while(T-- > 0) {
             int n = s.nextInt();
             int[] ar = new int[n];
             Long total = 0l;
-
-            for (int i = 0; i < ar.length; i++) {
+            for (int i = 0; i < ar.length; i++)
                 ar[i] = s.nextInt();
-            }
-            
-            for (int i = 1; i < ar.length; i++) {
+            for (int i = 1; i < ar.length; i++)
                 total += Long.valueOf(Math.abs(ar[i] - ar[i - 1]) - 1);                
-            }
-
             System.out.println(total);
-
         }
     }
 }
