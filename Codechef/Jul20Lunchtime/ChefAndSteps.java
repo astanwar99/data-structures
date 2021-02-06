8ff11bffbeffe85beb61b64dcfb8ec4e3ffd27d7
astanwar99 <ashwani@doselect.com>
2021-02-06 21:53:58 +0530

    Minor change

--- Codechef/Jul20Lunchtime/ChefAndSteps.java
+++ Codechef/Jul20Lunchtime/ChefAndSteps.java
@@ -4,6 +4,7 @@ public class ChefAndSteps{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int T = s.nextInt();
+        
         while(T-- > 0) {
             int n = s.nextInt();
             int k = s.nextInt();
