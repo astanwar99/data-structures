1e2d0631e124f685876eaa3433861ad4b41a0752
astanwar99 <ashwani@doselect.com>
2021-04-11 13:33:06 +0530

    Equal sum partition in go

---
+++ Go/equalSumPartion.go
@@ -0,0 +1,88 @@
+package main
+
+import "fmt"
+
+func recursiveEqualSumPart(arr []int, sum int, n int) bool {
+	if sum == 0 {
+		return true
+	}
+	if n == 0 {
+		return false
+	}
+
+	if arr[n-1] <= sum {
+		return recursiveEqualSumPart(arr, sum, n-1) ||
+			recursiveEqualSumPart(arr, sum-arr[n-1], n-1)
+	}
+	return recursiveEqualSumPart(arr, sum, n-1)
+}
+
+func memoizationEqualSumPart(arr []int, sum int, n int) int {
+	if T_int[n][sum] != -1 {
+		return T_int[n][sum]
+	}
+
+	if sum == 0 {
+		T_int[n][sum] = 1
+		return T_int[n][sum]
+	}
+
+	if n == 0 {
+		T_int[n][sum] = 0
+		return T_int[n][sum]
+	}
+
+	if arr[n-1] <= sum {
+		T_int[n][sum] = max(memoizationEqualSumPart(arr, sum, n-1),
+			memoizationEqualSumPart(arr, sum-arr[n-1], n-1))
+	} else {
+		T_int[n][sum] = memoizationEqualSumPart(arr, sum, n-1)
+	}
+	return T_int[n][sum]
+}
+
+func topdownEqualSumPart(arr []int, sum int, n int) bool {
+
+	for i := 0; i <= n; i++ {
+		T_bool[i][0] = true
+	}
+
+	for i := 1; i <= sum; i++ {
+		T_bool[sum][i] = false
+	}
+
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= sum; j++ {
+			if arr[i-1] <= j {
+				T_bool[i][j] = T_bool[i-1][j] || T_bool[i-1][j-arr[i-1]]
+			} else {
+				T_bool[i][j] = T_bool[i-1][j]
+			}
+		}
+	}
+	return T_bool[n][sum]
+}
+
+func equalSumPart() {
+	var arr = []int{2, 5, 8, 4, 9}
+	var sum int
+
+	for a := range arr {
+		sum += a
+	}
+
+	var n int = len(arr)
+
+	for i := 0; i <= n; i++ {
+		for j := 0; j <= sum; j++ {
+			T_int[i][j] = -1
+		}
+	}
+	if sum%2 == 1 {
+		fmt.Println(false)
+		return
+	}
+	fmt.Println(recursiveEqualSumPart(arr, sum/2, n))
+	fmt.Println(memoizationEqualSumPart(arr, sum/2, n))
+	fmt.Println(topdownEqualSumPart(arr, sum/2, n))
+}
---
+++ Go/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var T_bool [10][50]bool
+var T_int [10][50]int
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println("Knapsack 0-1:")
+	knapsack01()
+
+	fmt.Println("Subset Sum:")
+	subsetSum()
+
+	fmt.Println("Equal Sum Partition: ")
+	equalSumPart()
+	// for i := 0; i <= n; i++ {
+	// 	for j := 0; j <= sum; j++ {
+	// 		fmt.Print(T_bool[i][j], " ")
+	// 	}
+	// 	fmt.Println()
+	// }
+}
