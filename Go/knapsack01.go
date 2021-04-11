9b6e7c904a49ad0ed6cb1fc2174c8107c813635e
astanwar99 <ashwani@doselect.com>
2021-04-11 12:43:59 +0530

    go module structured

--- Go/knapsack01.go
+++ Go/knapsack01.go
@@ -5,15 +5,6 @@ import (
 	"time"
 )
 
-var T [10][50]int
-
-func max(a int, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 func recursiveKnapsack(val []int, wt []int, W int, n int) int {
 	if W == 0 || n == 0 {
 		return 0
@@ -28,46 +19,46 @@ func recursiveKnapsack(val []int, wt []int, W int, n int) int {
 }
 
 func knapsackMemoization(val []int, wt []int, W int, n int) int {
-	if T[n][W] != -1 {
-		return T[n][W]
+	if T_int[n][W] != -1 {
+		return T_int[n][W]
 	}
 
 	if n == 0 || W == 0 {
-		T[n][W] = 0
-		return T[n][W]
+		T_int[n][W] = 0
+		return T_int[n][W]
 	}
 
 	if wt[n-1] <= W {
-		T[n][W] = max(knapsackMemoization(val, wt, W, n-1),
+		T_int[n][W] = max(knapsackMemoization(val, wt, W, n-1),
 			knapsackMemoization(val, wt, W-wt[n-1], n-1)+val[n-1])
 	} else {
-		T[n][W] = knapsackMemoization(val, wt, W, n-1)
+		T_int[n][W] = knapsackMemoization(val, wt, W, n-1)
 	}
-	return T[n][W]
+	return T_int[n][W]
 
 }
 
 func knapsackTopdown(val []int, wt []int, W int, n int) int {
 	for i := 0; i <= W; i++ {
-		T[n][i] = 0
+		T_int[n][i] = 0
 	}
 	for i := 1; i <= n; i++ {
-		T[n][W] = 0
+		T_int[n][W] = 0
 	}
 
 	for i := 1; i <= n; i++ {
 		for j := 1; j <= W; j++ {
 			if wt[i-1] <= j {
-				T[i][j] = max(T[i-1][j-wt[i-1]]+val[i-1], T[i-1][j])
+				T_int[i][j] = max(T_int[i-1][j-wt[i-1]]+val[i-1], T_int[i-1][j])
 			} else {
-				T[i][j] = T[i-1][j]
+				T_int[i][j] = T_int[i-1][j]
 			}
 		}
 	}
-	return T[n][W]
+	return T_int[n][W]
 }
 
-func main() {
+func knapsack01() {
 	var val = []int{10, 15, 40, 40}
 	var wt = []int{1, 3, 4, 6}
 	var W int = 7
@@ -75,7 +66,7 @@ func main() {
 
 	for i := 0; i <= n; i++ {
 		for j := 0; j <= W; j++ {
-			T[i][j] = -1
+			T_int[i][j] = -1
 		}
 	}
 	start := time.Now()
@@ -85,10 +76,4 @@ func main() {
 	fmt.Println("Execution Time: ", time.Since(start))
 	fmt.Println(knapsackTopdown(val, wt, W, n))
 	fmt.Println("Execution Time: ", time.Since(start))
-	// for i := 0; i <= n; i++ {
-	// 	for j := 0; j <= W; j++ {
-	// 		fmt.Print(T[i][j], " ")
-	// 	}
-	// 	fmt.Println()
-	// }
 }
